cfcf720d085cf5a1d90ca9d330be91a1e346e670
Lucas Said <lsaid@spacedev.uy>
2020-12-27 16:07:17 -0300

    adding benefits

---
+++ data/maintenance/benefits.js
@@ -0,0 +1,11 @@
+import window from '../../public/icons/window.svg'
+
+const benefits = [
+  {
+    icon: window,
+    title: 'Plugin/Site up',
+    desc: 'We’re here to safetly install or update plugins and themes to keep your site up to date.'
+  },
+]
+
+export default benefits
