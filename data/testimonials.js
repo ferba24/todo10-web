81de4d2aa89ea2a1fd5411a545ad444c1fb2a79c
Fernando Basualdo <hello@fernandocb.com>
2021-03-29 01:46:40 -0300

    Testimonial added

--- data/testimonials.js
+++ data/testimonials.js
@@ -13,7 +13,7 @@ const testimonials = [
     image: '/images/alejandro-sanz.jpg',
     text: 'Here I was, 4 years ago with a 12 years old forum ruined by VBulletin 5, and then hacked. I cannot imagine worse conditions to start with. Ivan of Todo10 did everything to look so easy, we got rid of the hacker then migrated the broken VB5 forum to Xenforo. Since then they are around the clock for us solving every request I have. 24/7 of great professional service, by far the best I\'ve ever seen or heard of. They are simply incredible! Everything you read in other testimonials is true. Highly recommended!',
     extra: 'FLORIN BARBU. Warwickforum.com'
-  }
+  },
   {
     image: '/images/alejandro-sanz.jpg',
     text: 'After having some bad experiences with vBulletin, we decided to migrate our community forum from vB4 to Xenforo. Our forum has 1.1M+ posts and over 150.000 members, so we hired Todo10 for a professional migration services.\n' +
@@ -21,6 +21,16 @@ const testimonials = [
         'Also, we are very satisfied by the quality of the host provided by Todo10, it works great and we didn\'t have any problems so far!\n' +
         'I highly recommend Todo10 services, 5 ⭐!',
     extra: 'John Doe. Laleagane.ro'
+  },
+  {
+    image: '/images/alejandro-sanz.jpg',
+    text: 'They have migrated my vBulletin forum to Xenforo 2.0 with a minimum downtime in a worldwide pandemic crisis called COVID-19. A very good job for a wide community: 200.000 threads, 2.000.000 of post and several GB of data, photos, images. Hope to work in the next future with this very professonal Team.',
+    extra: 'Luca. Punterforum.com'
+  },
+  {
+    image: '/images/alejandro-sanz.jpg',
+    text: 'I really don t think there is a better partner for migration than Todo10. I have never seen such a friendly and patient company that just doesn\'t give up until the customer is happy and satisfied..',
+    extra: 'Anton. mMtoredbikes.com'
   },
 ]
 export default testimonials
