8d4ce3c8eb72366000eb403b01be8044c1c8bac2
Fernando Basualdo <hello@fernandocb.com>
2021-04-01 00:40:03 -0300

    Testimonials modified

--- data/testimonials.js
+++ data/testimonials.js
@@ -1,36 +1,33 @@
 const testimonials = [
   {
     image: '/images/alejandro-sanz.jpg',
-    text: "I've been utilizing the excellent services of Todo10.com for over eight years now and they are fantastic. Skilled in solving Forum and other problems and fast acting. I get a response immediately and soon after the problem or question is resolved.",
-    extra: 'ANDY GILCHRIST. AskAndyAboutClothes.com.'
+    text: "I've been utilizing the excellent services of Todo10.com for over eight years now and they are fantastic.",
+    extra: 'ANDY GILCHRIST - AskAndyAboutClothes.com.'
   },
   {
     image: '/images/alejandro-sanz.jpg',
-    text: 'I have been using Todo10.com for ongoing monthly maintenance, add ons installation, and more. In terms of your day to day wants and needs for a forum, they are the best. When you have a big board or multiple boards, having a pro that knows your server is key. Their server knowledge and general can-do attitude when it comes to add-ons makes it possible for me to focus on the business of my forum. Responsive, reliable, and affordable. All the best to you Claudio and Javier!',
-    extra: 'ROBERT PAVLICK. EnthusedDigital.com'
+    text: ' Their server knowledge and general can-do attitude makes it possible for me to focus on the business of my forum. Responsive, reliable, and affordable.',
+    extra: 'ROBERT PAVLICK - EnthusedDigital.com'
   },
   {
     image: '/images/alejandro-sanz.jpg',
-    text: 'Here I was, 4 years ago with a 12 years old forum ruined by VBulletin 5, and then hacked. I cannot imagine worse conditions to start with. Ivan of Todo10 did everything to look so easy, we got rid of the hacker then migrated the broken VB5 forum to Xenforo. Since then they are around the clock for us solving every request I have. 24/7 of great professional service, by far the best I\'ve ever seen or heard of. They are simply incredible! Everything you read in other testimonials is true. Highly recommended!',
-    extra: 'FLORIN BARBU. Warwickforum.com'
+    text: 'Todo10 did everything to look so easy. 24/7 of great professional service, by far the best I\'ve ever seen or heard of. They are simply incredible! Everything you read in other testimonials is true. Highly recommended!',
+    extra: 'FLORIN BARBU - Warwickforum.com'
   },
   {
     image: '/images/alejandro-sanz.jpg',
-    text: 'After having some bad experiences with vBulletin, we decided to migrate our community forum from vB4 to Xenforo. Our forum has 1.1M+ posts and over 150.000 members, so we hired Todo10 for a professional migration services.\n' +
-        'We were very impressed by how Fernando managed to upgrade our forums. He was able to fix any error during the process and migrate our forum in a very short time, which it was great because we didn\'t want a long downtime.\n' +
-        'Also, we are very satisfied by the quality of the host provided by Todo10, it works great and we didn\'t have any problems so far!\n' +
-        'I highly recommend Todo10 services, 5 ⭐!',
-    extra: 'John Doe. Laleagane.ro'
+    text: 'We hired Todo10 for a professional migration services. Also, we are very satisfied by the quality of the host. I highly recommend Todo10 services, 5 ⭐!',
+    extra: 'John Doe - Laleagane.ro'
   },
   {
     image: '/images/alejandro-sanz.jpg',
-    text: 'They have migrated my vBulletin forum to Xenforo 2.0 with a minimum downtime in a worldwide pandemic crisis called COVID-19. A very good job for a wide community: 200.000 threads, 2.000.000 of post and several GB of data, photos, images. Hope to work in the next future with this very professonal Team.',
+    text: 'They have migrated my vBulletin forum to Xenforo 2.0 with a minimum downtime in a worldwide pandemic crisis called COVID-19. A very good job for a wide community: 2.000.000 posts and several GB of data. Hope to work in the next future with this very professonal Team.',
     extra: 'Luca. Punterforum.com'
   },
   {
     image: '/images/alejandro-sanz.jpg',
-    text: 'I really don t think there is a better partner for migration than Todo10. I have never seen such a friendly and patient company that just doesn\'t give up until the customer is happy and satisfied..',
-    extra: 'Anton. mMtoredbikes.com'
+    text: 'I really don t think there is a better partner for migration than Todo10. I have never seen such a friendly and patient company that just doesn\'t give up until the customer is happy and satisfied.',
+      extra: 'Anton - Motoredbikes.com'
   },
 ]
 export default testimonials
