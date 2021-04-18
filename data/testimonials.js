e4faba35aa8fa94144663c3f1e61dd5b834b60b7
Fernando Basualdo <hello@fernandocb.com>
2021-04-17 23:21:03 -0300

    [EDIT] Testimonial img path

--- data/testimonials.js
+++ data/testimonials.js
@@ -1,31 +1,31 @@
 const testimonials = [
   {
-    image: '/images/alejandro-sanz.jpg',
+    image: '/images/testimonials/alejandro-sanz.jpg',
     text: "I've been utilizing the excellent services of Todo10.com for over eight years now and they are fantastic.",
     extra: 'ANDY GILCHRIST - AskAndyAboutClothes.com.'
   },
   {
-    image: '/images/alejandro-sanz.jpg',
+    image: '/images/testimonials/alejandro-sanz.jpg',
     text: ' Their server knowledge and general can-do attitude makes it possible for me to focus on the business of my forum. Responsive, reliable, and affordable.',
     extra: 'ROBERT PAVLICK - EnthusedDigital.com'
   },
   {
-    image: '/images/alejandro-sanz.jpg',
+    image: '/images/testimonials/alejandro-sanz.jpg',
     text: 'Todo10 did everything to look so easy. 24/7 of great professional service, by far the best I\'ve ever seen or heard of. They are simply incredible! Everything you read in other testimonials is true. Highly recommended!',
     extra: 'FLORIN BARBU - Warwickforum.com'
   },
   {
-    image: '/images/alejandro-sanz.jpg',
+    image: '/images/testimonials/alejandro-sanz.jpg',
     text: 'We hired Todo10 for a professional migration services. Also, we are very satisfied by the quality of the host. I highly recommend Todo10 services, 5 ⭐!',
     extra: 'John Doe - Laleagane.ro'
   },
   {
-    image: '/images/alejandro-sanz.jpg',
-    text: 'They have migrated my vBulletin forum to Xenforo 2.0 with a minimum downtime in a worldwide pandemic crisis called COVID-19. A very good job for a wide community: 2.000.000 posts and several GB of data. Hope to work in the next future with this very professonal Team.',
+    image: '/images/testimonials/alejandro-sanz.jpg',
+    text: 'They have migrated my vBulletin forum to Xenforo 2.0 with a minimum downtime . A very good job for a wide community: 2M posts and several GB of data. Hope to work in the next future with this very professional Team.',
     extra: 'Luca. Punterforum.com'
   },
   {
-    image: '/images/alejandro-sanz.jpg',
+    image: '/images/testimonials/alejandro-sanz.jpg',
     text: 'I really don t think there is a better partner for migration than Todo10. I have never seen such a friendly and patient company that just doesn\'t give up until the customer is happy and satisfied.',
       extra: 'Anton - Motoredbikes.com'
   },
